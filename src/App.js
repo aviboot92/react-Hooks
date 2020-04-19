dc04217ca848a4f3e5851501e1b9cde6fabade68
Venkat Avinash Karra <akarra@uxli.co.uk>
2020-04-19 10:38:17 +0530

    use State hooks are added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    My React HOOKS</h1>
+            </header>
+            <NotesDisplay/>
+        </div>
+    );
+}
+
+const NotesDisplay = () => {
+    const [notes,
+        setNotes] = useState([]);
+    const [title,
+        setTitle] = useState('');
+    const [descreption,
+        setDescreption] = useState('');
+
+    const onInputChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        switch (name) {
+            case 'title':
+                return setTitle(value);
+                break;
+
+            case 'descreption':
+                return setDescreption(value);
+                break;
+
+            default:
+                return null;
+        }
+    }
+
+    return (
+        <Fragment>
+            <Note notes=""/>
+            <div className="inputGrabber">
+              <h3>Add NOTE:</h3>
+              <form>
+                <label for='name'>Title:
+                </label>
+                <input type="text" id='name' name='title' placeholder='Add TITLE' onChange={onInputChange}/>
+                <br/>
+                <label for='descreption'>Descreption:
+                </label>
+                <textarea
+                    id='descreption'
+                    name='descreption'
+                    onChange={onInputChange}
+                    rows='4'
+                    cols='50'
+                    placeholder='Please add your descretion here'></textarea>
+                    <br />
+                    <button type='submit'>ADD NOTE</button>
+                </form>
+            </div>
+        </Fragment>
+    )
+}
+
+const Note = (props) => {
+    return (
+        <Fragment>
+            <h3>NOTES:</h3>
+        </Fragment>
+    )
 }
 
 export default App;
